3d3463b88728b37d5b642cb447b1c734c2d9f7e5
stiago <stiago01r@gmail.com>
2019-07-15 15:51:18 -0500

    esqueleto en angular

--- platzinger/src/app/app.module.ts
+++ platzinger/src/app/app.module.ts
@@ -3,33 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
-import { ConversationComponent } from './conversation/conversation.component';
-import { ProfileComponent } from './profile/profile.component';
-import { Routes, RouterModule } from '@angular/router';
-import { MenuComponent } from './menu/menu.component';
-//direccinador 
-const appRoutes: Routes=[
-  {path: '', component : HomeComponent},
-  {path: 'home', component : HomeComponent},
-  {path: 'login', component : LoginComponent},
-  {path: 'conversation/:uid',component : ConversationComponent},
-  {path: 'profile',component: ProfileComponent}
-];
+
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    HomeComponent,
-    ConversationComponent,
-    ProfileComponent,
-    MenuComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    RouterModule.forRoot(appRoutes)
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
