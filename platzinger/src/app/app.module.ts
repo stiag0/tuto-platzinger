8fb5016f560e79337379395e113a26869ee37898
stiago <stiago01r@gmail.com>
2019-07-16 14:53:44 -0500

    implementacion de las rutas con el RouterModule

--- platzinger/src/app/app.module.ts
+++ platzinger/src/app/app.module.ts
@@ -3,13 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { ConversationComponent } from './conversation/conversation.component';
+import { ProfileComponent } from './profile/profile.component';
+import { Routes, RouterModule } from '@angular/router';
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'conversation', component: ConversationComponent},
+  {path: 'profile', component: ProfileComponent}
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    ConversationComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(appRoutes),
     AppRoutingModule
   ],
   providers: [],
